e2b0b6fbe0fabd5bcb3675c7b59aced43e5e0053
Mohan-444 <chinnamohan1998@gmail.com>
2025-06-27 10:57:27 +0530

    create S3

--- s3-static.tf
+++ s3-static.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "GA-static-website-bucket" {
-  bucket = "static-website-bucket"
+resource "aws_s3_bucket" "GA-mohan-static-website-bucket" {
+  bucket = "GA-mohan-static-website-bucket"
   region="us-east-1"
 
   tags = {
